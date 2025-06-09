2f2ec812e134d35e6e3aea41fcec00eb438be64d
skullcrazy65 <126969244+skullcrazy65@users.noreply.github.com>
2025-06-09 23:21:20 +0200

    Updated NotificationWrapper.tsx

--- web/src/features/notifications/NotificationWrapper.tsx
+++ web/src/features/notifications/NotificationWrapper.tsx
@@ -185,7 +185,7 @@ const Notifications: React.FC = () => {
                 {data.showDuration ? (
                   <RingProgress
                     key={toastKey}
-                    size={16}
+                    size={26}
                     thickness={3}
                     sections={[{ value: 100, color: iconColor }]}
                     style={{ alignSelf: !data.alignIcon || data.alignIcon === 'center' ? 'center' : 'start' }}
